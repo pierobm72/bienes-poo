5ac49a67bdf8db0d1a3629bf817746e83330d5ba
Piero <pierobm72@gmail.com>
2023-02-17 16:16:49 -0500

    CRUD: Insertar los datos a la base de datos

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -1,11 +1,36 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . "/rutas.php";
-
-require RUTA_BASEDATOS;
-$db = conectarDB();
-var_dump($db);
-
 include_once RUTA_FUNCIONES;
+require RUTA_BASEDATOS;
+
+//Conectarse a la base de datos
+$db = conectarDB();
+
+//Verificar que la peticion de datos sea de tipo POST
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+  //Almacenar los datos ingresados por el usuario en el formulario
+  $titulo = $_POST["titulo"];
+  $precio = $_POST["precio"];
+  $descripcion = $_POST["descripcion"];
+  $habitacion = $_POST["habitacion"];
+  $wc = $_POST["wc"];
+  $estacionamiento = $_POST["estacionamiento"];
+  $vendedor_id = $_POST["vendedor_id"];
+
+  //Almacenar los datos en una query
+  $query = "INSERT INTO propiedades (titulo,precio,descripcion,habitacion,wc,estacionamiento,vendedor_id) ";
+  $query .= "VALUES ('$titulo','$precio','$descripcion','$habitacion','$wc','$estacionamiento','$vendedor_id');";
+
+  //Insertar la consulta a la base de datos
+  $resultado = mysqli_query($db,$query);
+
+  //Validar que la consulta se ha enviado
+  if($resultado){
+    echo "Insertado correctamente";
+  }
+}
+
 incluirTemplates("header");
 ?>
 
@@ -13,41 +38,41 @@ incluirTemplates("header");
     <h1>Crear</h1>
     <a href="/admin/" class="boton boton-verde">Volver</a>
 
-    <form class="formulario">
+    <form class="formulario" action="/admin/propiedades/crear.php" method="POST">
       <fieldset>
           <legend>Informacion General</legend>
 
           <label for="titulo">Titulo:</label>
-          <input type="text" id="titulo" placeholder="Titulo Propiedad">
+          <input type="text" id="titulo" placeholder="Titulo Propiedad" name="titulo">
 
           <label for="precio">Precio:</label>
-          <input type="text" id="precio" placeholder="Titulo Precio">
+          <input type="number" id="precio" placeholder="Titulo Precio" name="precio">
 
           <label for="imagen">Imagen:</label>
           <input type="file" id="imagen"  accept="image/jpeg, image/png">
 
           <label for="descripcion">Descripcion:</label>
-          <textarea id="descripcion" ></textarea>
+          <textarea id="descripcion" name="descripcion"></textarea>
 
       </fieldset>
 
       <fieldset>
           <legend>Informacion Propiedad</legend>
 
-          <label for="habitaciones">Habitaciones:</label>
-          <input type="number" min="1" max="9" id="habitaciones" placeholder="Ejm. 3">
+          <label for="habitacion">Habitaciones:</label>
+          <input type="number" min="1" max="9" id="habitacion" placeholder="Ejm. 3" name="habitacion">
 
           <label for="wc:">Baños:</label>
-          <input type="number" min="1" max="9" id="wc:" placeholder="Ejm. 3">
+          <input type="number" min="1" max="9" id="wc:" placeholder="Ejm. 3" name="wc">
 
           <label for="estacionamiento:">Estacionamiento:</label>
-          <input type="number" min="1" max="9" id="estacionamiento:" placeholder="Ejm. 3">          
+          <input type="number" min="1" max="9" id="estacionamiento:" placeholder="Ejm. 3" name="estacionamiento">          
 
       </fieldset>
 
       <fieldset>
         <legend>Vendedor</legend>
-        <select>
+        <select name="vendedor_id">
             <option value="1">Juan</option>
             <option value="2">Karen</option>
         </select>
--- includes/funciones.php
+++ includes/funciones.php
@@ -1,7 +1,42 @@
-<?php 
+<?php
 //Incluir archivo que contiene las rutas de los directorios
-include_once ($_SERVER['DOCUMENT_ROOT'] . "/rutas.php"); 
+include_once($_SERVER['DOCUMENT_ROOT'] . "/rutas.php");
 
-function incluirTemplates(string $nombre, bool $inicio = false){
+function incluirTemplates(string $nombre, bool $inicio = false)
+{
   include RUTA_TEMPLATES . "/{$nombre}.php";
+}
+
+
+// Imprime los arreglos mas bonitos
+function prettyPrint($mensaje, $modo = 0)
+{
+  if ($modo == 0) {
+    echo "<pre>";
+    var_export($mensaje);
+    echo "</pre>";
+  } else {
+    echo "<pre>";
+    var_dump($mensaje);
+    echo "</pre>";
+  }
+
+
+  //Imprimir array en la consola
+  $object = json_encode($mensaje);
+  print_r('<script>console.log(' . $object . ')</script>');
+}
+
+function debuguear($mensaje, $modo = 0)
+{
+  if ($modo == 0) {
+    echo "<pre>";
+    var_dump($mensaje);
+    echo "</pre>";
+    exit();
+  } else {
+    echo "<pre>";
+    var_dump($mensaje);
+    echo "</pre>";
+  }
 }
